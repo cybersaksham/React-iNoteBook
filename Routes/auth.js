be66efe09b7718af828fa8cf08969ea34af33886
Saksham <2020UCP1801@mnit.ac.in>
2021-09-02 16:20:12 +0530

    user added to database

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const User = require("../Models/User");
 
 const router = express.Router();
 
 router.post("/createUser", (req, res) => {
+  const user = User(req.body);
+  user.save();
   res.json(req.body);
 });
 
