0c94e29d8732de72e45bf073b8ea9cf096508cf6
Saksham <2020UCP1801@mnit.ac.in>
2021-09-02 16:38:38 +0530

    email unique checked

--- Routes/auth.js
+++ Routes/auth.js
@@ -14,14 +14,22 @@ router.post(
       max: 10,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const user = User(req.body);
-    user.save();
-    res.json(req.body);
+    let user = await User.findOne({ email: req.body.email });
+    if (user)
+      return res
+        .status(400)
+        .json({ error: "User already exists with this email id." });
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json(user);
   }
 );
 
