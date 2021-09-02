1f41475a5ea11c8313d73d66273c3b570c3606c4
Saksham <2020UCP1801@mnit.ac.in>
2021-09-02 16:27:15 +0530

    auth validators made

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const User = require("../Models/User");
 
 const router = express.Router();
 
-router.post("/createUser", (req, res) => {
-  const user = User(req.body);
-  user.save();
-  res.json(req.body);
-});
+router.post(
+  "/createUser",
+  [
+    body("name", "Name must be 3 characters long.").isLength({ min: 3 }),
+    body("email", "Enter a valid email.").isEmail(),
+    body("password", "Password must be between 5 & 10 character.").isLength({
+      min: 5,
+      max: 10,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const user = User(req.body);
+    user.save();
+    res.json(req.body);
+  }
+);
 
 module.exports = router;
