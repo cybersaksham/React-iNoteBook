8e45b9dcc7a523fd0f047c01f8b53bcd8a2a385d
Saksham <2020UCP1801@mnit.ac.in>
2021-09-04 20:49:07 +0530

    fetch notes endpoint

--- Routes/notes.js
+++ Routes/notes.js
@@ -5,4 +5,15 @@ const { Router } = require("express");
 
 const router = express.Router();
 
+// Fetch All Notes
+router.get("/", fetchUser, async (req, res) => {
+  try {
+    // Finding Notes
+    const notes = await Notes.find({ user: req.user.id });
+    return res.json(notes);
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
