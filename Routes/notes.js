4f4e03bdf57fc65b656bcfe4529937f78129f7d9
Saksham <2020UCP1801@mnit.ac.in>
2021-09-04 20:57:41 +0530

    add notes endpoint

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const Notes = require("../Models/Notes");
 const fetchUser = require("../MiddleWare/fetch-user");
 const { Router } = require("express");
@@ -15,5 +16,39 @@ router.get("/", fetchUser, async (req, res) => {
     return res.status(500).send("Internal Server Error");
   }
 });
+
+// Create New Note
+router.post(
+  "/",
+  fetchUser,
+  [
+    // Validators
+    body("title", "Title must be 3 characters long.").isLength({ min: 3 }),
+    body("description", "Description must be 5 characters long.").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    // Sending error if validator failed
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { title, description, tag } = req.body;
+
+      // Creating Note
+      const note = new Notes({ title, description, tag, user: req.user.id });
+
+      // Savign Note
+      const savedNote = await note.save();
+
+      return res.json(savedNote);
+    } catch (error) {
+      return res.status(500).send("Internal Server Error");
+    }
+  }
+);
 
 module.exports = router;
